afc6edd5cd76392f706ed1d6f4ce4a0956daf54d
许圣峰 <kricsleo@163.com>
2023-06-28 19:48:06 +0800

    chore: init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-export const one = 1
-export const two = 2
+import { Options as SearchOptions } from 'file-text-search/types'
+import { search } from 'file-text-search'
+import tinyfy from 'tinify'
+
+const fileSeachOptions: SearchOptions = {
+  include: [],
+  search: [/https:\/\/.*(?=('|"))/]
+}
+
+interface Options extends SearchOptions {
+  tinypngKey: string
+}
+
+async function run(options: Options) {
+  const matches = await search(fileSeachOptions)
+  const urls = Array.from(new Set(matches.map(t => t.matches.map(k => k.match)).flat()))
+  tinyfy.key = options.tinypngKey
+  await Promise.all(urls.map(async url => {
+    const source = tinyfy.fromUrl(url)
+    // todo: generate img name to write img
+    await source.toFile(Math.random() + '.png')
+    // todo: check compress size
+    // todo: upload img after tinyfied
+  }))
+}
