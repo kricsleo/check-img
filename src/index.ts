9e598768ea9d74a56ec8f94587d421749699601e
许圣峰 <kricsleo@163.com>
2023-06-28 20:06:14 +0800

    WIP: fetch img meta

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,18 @@ const fileSeachOptions: SearchOptions = {
   search: [/https:\/\/.*(?=('|"))/]
 }
 
-interface Options extends SearchOptions {
-  tinypngKey: string
-  minCompressRatio: number
-}
-
 async function run(options: Options) {
   const matches = await search(fileSeachOptions)
   const urls = Array.from(new Set(matches.map(t => t.matches.map(k => k.match)).flat()))
   tinyfy.key = options.tinypngKey
   await Promise.all(urls.map(async url => {
     const source = tinyfy.fromUrl(url)
-    const buffer = await source.toBuffer()
+    const [ buffer, meta ] = await Promise.all([
+      await source.toBuffer(),
+      fetchImgMeta(url)
+    ])
     // todo: check compress size
-    const result = source.result()
+    const result = source.result().size
     console.log('result', result)
     const compressSize = 0.2
     const shouldReplace = compressSize > options.minCompressRatio
@@ -30,6 +28,25 @@ async function run(options: Options) {
   }))
 }
 
+async function fetchImgMeta(url: string) {
+  // https://help.aliyun.com/document_detail/44975.html?spm=a2c4g.44688.0.0.61a54b9bBlH4JX
+  const metaUrl = url + '?x-oss-process=image/info'
+  const aliyunMeta: AliyunImgMeta = await (await fetch(metaUrl)).json()
+  const meta = { fileSize: Number(aliyunMeta.FileSize.value) }
+  return meta
+}
+
 async function uploadImg(buffer: Uint8Array) {
   // todo: upload img
+}
+
+
+interface Options extends SearchOptions {
+  tinypngKey: string
+  minCompressRatio: number
+}
+
+// https://image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg?x-oss-process=image/info
+interface AliyunImgMeta {
+  FileSize: { value: string }
 }
